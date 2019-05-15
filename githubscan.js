3f56dcbc5470fac1357faaaa1d011941395934e9
Floris Knol <fknol@codegorilla.nl>
2019-05-15 20:45:55 +0200

    Iterate all repos of all users and count commits for author

--- githubscan.js
+++ githubscan.js
@@ -5,6 +5,40 @@ var gitusers = [	'bashir-panjshiri','EvertdeVries','Callisto79','R3NOCP','erwins
 					'Aletta104','drohnwynandrt','OHiddema','SamirHartlief','Aarnoud-Meijer','DJLemstra',
 					'JaccoGritter','LanaSijsling','jdereus87','supersmitty2018','IvoJongmans'	];
 
+var commitmap = new Map();
+var repomap = new Map();
+
+
+function countCommits(susername, sreponame) {
+
+
+	//api.github.com/repos/:user/repositoryNameFromArray/commits?author=:user 
+  $.ajax({
+        url: "https://api.github.com/repos/" + susername  + "/" + sreponame + "/commits?author="  + susername,
+    	
+        // url: "https://api.github.com/rate_limit",
+        headers: { Authorization: "Basic " + GITAUTH },
+		success: function(result){
+			
+				console.log(susername + "." + sreponame + " : " + result.length);
+				let oldcommits = 0;
+				if (commitmap.get(susername) != null)
+				  oldcommits =  commitmap.get(susername);
+
+				commitmap.set(susername, oldcommits + result.length);
+
+		
+				}
+
+			//$('#div1').html( $('#div1').html() + "<BR>" + currentuser + ": " + result.length); 
+      		
+    	});
+
+
+
+}
+
+
 
 $(document).ready(function(){
     
@@ -25,6 +59,20 @@ $(document).ready(function(){
 			success: function(result){
 
 				console.log(result);
+
+				for (var gitrepo of result) {
+					let currentreponame = gitrepo.name;
+
+					let oldrepos = 0;
+					if (repomap.get(currentuser) != null)
+					  oldrepos =  repomap.get(currentuser);
+
+					repomap.set(currentuser, oldrepos + 1);
+
+					//console.log(currentrepo.name);
+					countCommits(currentuser, currentreponame);
+				}
+
 	      		
 	    	}
 
